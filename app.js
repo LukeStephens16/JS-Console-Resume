22960048b0d193af4fcaf01cfc25a4341200366e
unknown <lukestephens16@gmail.com>
2021-05-13 10:40:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+let fullName = 'Name: LUKE STEPHENS';
+    console.log(fullName)
+
+let Career = 'Career: Web Designer'; 
+    console.log(Career)
+
+
+let Description = 'Description: I like long walks on the beach'; 
+    console.log(Description)
+
+
+let Myinterest = [' ', 'My Interest:', 'Gaming', 'Running', 'Watching Films', 'Relaxing']; {
+    console.log(Myinterest[0]);
+    console.log(Myinterest[1]);
+    console.log('* ' + Myinterest[2]);
+    console.log('* ' + Myinterest[3]);
+    console.log('* ' + Myinterest[4]);
+    console.log('* ' + Myinterest[5]);
+}
+
+let Mypreviousexp = [' ', 'My Previous Experience:', 'Worked at a fancy restaurant', 'Worked at a not so fancy restaurant', 'Worked at a nice golf store', 'Worked at a not so nice golf store']; {
+    console.log(Mypreviousexp[0]);
+    console.log(Mypreviousexp[1]);
+    console.log('* ' + Mypreviousexp[2]);
+    console.log('* ' + Mypreviousexp[3]);
+    console.log('* ' + Mypreviousexp[4]);
+    console.log('* ' + Mypreviousexp[5]);
+}
+
+let Myskills = [' ', 'My Skills:', 'HTML', 'CSS', 'JavaScript', 'BAM: Killin em with kindness']; {
+    console.log(Myskills[0]);
+    console.log(Myskills[1]);
+    console.log('* ' + Myskills[2]);
+    console.log('* ' + Myskills[3]);
+    console.log('* ' + Myskills[4]);
+    console.log('* ' + Myskills[5]);
+}
